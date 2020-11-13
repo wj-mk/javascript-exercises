b7c1f40f36818f11dd2f04753ba71e51bec4dad3
wjmk <williamkerley@protonmail.com>
2020-11-13 18:08:41 +0000

    completed removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    
+    // remove elements from an array
+    let elems = Array.from(elements);
+    
+    for (var i=0; i < elems.length; i++) {
+        
+        // locate and remove element
+        let loc = array.indexOf(elems[i]);
+        
+        if (loc == -1) {
+            continue;
+        }
+        else {
+            array.splice(loc, 1);
+        }
+    }
+    
+    return array;
 }
 
 module.exports = removeFromArray
