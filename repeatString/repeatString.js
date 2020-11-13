4b41d5e157bd9714441d4ec2461fd954bfe97979
wjmk <williamkerley@protonmail.com>
2020-11-13 15:16:22 +0000

    Completed helloWorld and repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
+    if (repeats < 0) {
+        return 'ERROR';
+    }
+    else {
+        let run = 0;
+        let output = '';
 
+        while (run < repeats) {
+            output += string;
+            run += 1;
+        };
+
+        return output;
+    }
 }
 
 module.exports = repeatString
