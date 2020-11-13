e45cc9ae95c1f476d974cb1ddc23799910bd9946
wjmk <williamkerley@protonmail.com>
2020-11-13 19:09:52 +0000

    completed tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(farenheit) {
 
+  let celcius = (farenheit - 32) * (5 / 9);
+
+  return Math.round(celcius*10) / 10;
 }
 
-const ctof = function() {
+const ctof = function(celcius) {
+  
+  let farenheit = (celcius  * 9 / 5) + 32;
 
+  return Math.round(farenheit * 10) / 10;
 }
 
 module.exports = {
