ee5151915f5f2331535187b1c96a39b19cc3881e
wjmk <williamkerley@protonmail.com>
2020-11-13 15:27:03 +0000

    Completed reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let index = string.length - 1;
+    let reverse = '';
 
+    while (index >= 0) {
+        reverse += string.charAt(index)
+        index -= 1;
+    };
+
+    return reverse;
 }
 
 module.exports = reverseString
